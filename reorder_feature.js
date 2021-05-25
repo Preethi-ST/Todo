71ec1088f2dc68aeacdc129b09eb9907421f2f1b
Preethi <78039676+Preethi-ST@users.noreply.github.com>
2021-05-25 23:49:30 +0530

    Update reorder_feature.js

--- reorder_feature.js
+++ reorder_feature.js
@@ -1,19 +1,17 @@
-
 const myarea = document.querySelector(".sortlist");
-       // console.log("in script");
-        new Sortable(myarea,{
-            animation : 350
+new Sortable(myarea,{
+    animation : 350
+});
+//update sorted elements in LS      
+document.querySelector(".sortlist").addEventListener('drop',function(){
+    console.log("fcjnsdmcx, ");
+    if(document.querySelector('.mylist li') !== null){
+        let myReorderedList = document.querySelectorAll('.mylist li');
+        console.log(myReorderedList);
+        localStorage.clear();
+        myReorderedList.forEach(task => {
+            //console.log(task.innerText);
+             saveTaskInLocalStorage(task.innerText) 
         });
-        document.addEventListener('dragleave',function(){
-        if(document.querySelector('.mylist li') !== null){
-            document.querySelector('.mylist li').addEventListener('drop',function(){
-                let myReorderedList = document.querySelectorAll('.mylist li');
-            console.log(myReorderedList);
-            localStorage.clear();
-            myReorderedList.forEach(task => {
-                console.log(task.innerText);
-                saveTaskInLocalStorage(task.innerText) });
-            })
-        }
-    })
-      
+    }
+})
